093d6dff6664d0066c1515638abe2b871f6726c4
papawolf <wolfcorp@me.com>
2020-02-08 00:52:38 +0000

    npm and require jQuery, jsdom make css more purple make jumbotron fluid more smooth img thumbnail add jquery nav item is active moving link bottom of body to head

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-
+var $ = require("jquery");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended : false}));
