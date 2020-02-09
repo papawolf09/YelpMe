2ab4cbf81c52ab8777510fda01c64f9fd88fe1df
papawolf <wolfcorp@me.com>
2020-02-09 13:52:46 +0000

    npm install mongoose

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var $ = require("jquery");
+var mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended : false}));
